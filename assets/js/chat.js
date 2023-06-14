33efc52d6b6709136763defe0fcb5574ec6fec82
markbaldoz <28640033+markbaldoz@users.noreply.github.com>
2023-06-14 11:24:11 +0800

    source

---
+++ assets/js/chat.js
@@ -0,0 +1,137 @@
+var questions = [
+    {
+        question: 'what is prdp scale up',
+        answer: ['The Philippine Rural Development Project '+
+                 '(PRDP) Scale-Up is one of the flagship '+
+                 'programs of the Department of Agriculture '+
+                 'designed to address the gaps in the '+
+                 'commodity value chains by improving the '+
+                 'efficiency of the food supply chain towards '+
+                 'greater connectivity, mobility, accessibility, '+
+                 'availability, and affordability of food in the '+
+                 'market.']
+    },
+    {
+        question: 'What is the aim of the PRDP Scale Up',
+        answer: ['The PRDP Scale-Up aims to contribute '+
+                 'to developing the sector by improving '+
+                 'farmers’ and fisherfolk’s access to markets '+
+                 'and increasing income from selected agri-'+
+                 'fishery value chains.']
+    },
+    {
+        question: 'Where will the PRDP Scale Up be implemented?',
+        answer: ['The PRDP Scale-Up covers all 16 regions composed of 82 provinces in the country.']
+    }, 
+    {
+        question: 'How does the PRDP Scale Up differ from the PRDP Original Loan, AF1, and AF2',
+        answer : [
+                 'The Philippine Rural Development Project '+
+                 'has evolved over the years, responding to '+
+                 'the needs and peculiarities of challenges '+
+                 'and demands of the agri-fishery sector. The '+
+                 'PRDP Original Loan was implemented in '+
+                 '2014. It received its first additional financing '+
+                 'in 2018. The Original Loan and the First '+
+                 'Additional Financing were committed to '+
+                 'achieving increased productivity, incomes, '+
+                 'and climate resiliency in rural communities. '+
+                 'The PRDP’s second additional financing '+
+                 'received in 2022 was retrofitted to address '+  
+                 'the demands of the New Normal post-'+
+                 'COVID pandemic. It supports agri-fishery '+
+                 'recovery and stimulus programs (PLANT, '+
+                 'PLANT, PLANT, KADIWA, etc.) of the DA '+
+                 'through interventions such as logistics and '+
+                 'food access.',
+                 
+                 // second paragraph
+                 'Considered a new project, the PRDP Scale- '+
+                 'Up will address the persistent challenges '+
+                 'besetting the sector. Notably, the project '+
+                 'will highlight clustering and consolidation '+
+                 'as a strategy, enhanced participation of '+
+                 'the private sector, and climate resiliency '+
+                 'of project interventions. In a nutshell, the '+
+                 'PRDP Scale-Up will be a bigger, expanded '+
+                 'version of the original project.'
+                ]
+    },
+    {
+        question : 'Who are the beneficiaries of the PRDP Scale Up',
+        answer : ['Actors within the commodity value '+
+                  'chains such as smallholder farmers '+
+                  'and fisher groups, farmers and fishers’ '+
+                  'cooperatives and associations (FCAs), '+
+                  'clusters of FCAs, producers, associations, '+
+                  'federations, processors, consolidators, food '+
+                  'manufacturers, and other players within the '+
+                  'value chains will directly benefit from the '+
+                  'project. They are also eligible proponents '+
+                  'of enterprise investments in partnership '+
+                  'with the private sector. Moreover, the Local '+
+                  'Government Units (LGUs) at the provincial, '+
+                  'municipal, and city levels as primary project '+
+                  'implementers will benefit the project and are '+
+                  'eligible proponents for infrastructure and '+
+                  'enterprise investments.']
+    },
+    {
+        question : 'Who are the partners in implementing the PRDP Scale Up',
+        answer : ['The project will partner with the private '+
+                  'sector for enterprise development. These '+
+                  'are private business entities that are willing '+
+                  'to enter into a marketing contract or buy- '+
+                  'back agreement with partner FCAs/FCA '+
+                  'cluster. They can provide cash/in-kind '+
+                  'investments such as equipment, facilities, or '+
+                  'infrastructure to the SPs. Relevant national '+
+                  'government agencies such as the DPWH, '+
+                  'DTI, DILG, DAR, and DENR, and regional '+
+                  'administrative bodies such as BARMM, '+
+                  'MinDA, and others are considered partners '+
+                  'in the project implementation through '+
+                  'leveraging and convergence initiatives. '+
+                  'Financing institutions (e.g. Landbank of the '+
+                  'Philippines, DBP, ACPC, etc.) and insurance '+
+                  'companies (e.g. PCIC) will be tapped to '+
+                  'help FCAs/cluster FCAs with their financing '+
+                  'requirement and insurance coverage. '+
+                  'Within the DA, the Project will forge closer '+
+                  'coordination with BAFE, CRAO, BSWM, '+
+                  'Banner Programs, AMAS, PhilMECH, and '+
+                  'PDS-PPP, among others.']
+    },
+    {
+        question: 'when will the prdp scale up be implemented',
+        answer: ['The PRDP Scale-Up is targeted to be implemented for six years from 2023 to 2029.']
+    },
+    {
+        question : 'Who are the FCAs and FCA clusters',
+        answer : [
+                'FCA (Farmers Fisherfolk Cooperatives and Associations)',
+
+                'FCA refers to farmers and fisherfolk '+
+                'cooperatives, associations, or non- '+
+                'stock corporations duly registered with '+
+                'appropriate government agencies (i.e. '+
+                'CDA, SEC, DOLE-BRW), and which are '+
+                'composed primarily of small agricultural '+
+                'producers, farmers, farm workers, '+
+                'agrarian reform beneficiaries, and '+
+                'fisherfolk who voluntarily join together '+
+                'to form business enterprises.',
+
+                'FCA Cluster',
+
+                'FCA Cluster shall be defined as a group '+
+                'of crops, livestock and/or fish producers '+
+                'within a community or adjacent '+
+                'communities on the basis of proximity '+
+                'of their production areas; similarity of '+
+                'inputs; and shared production activities/ '+
+                'processes and/or common final '+
+                'products.'
+        ]
+    }
+]
